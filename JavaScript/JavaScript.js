47aad2408305b6fc300d5c063b9a1cad822b6804
Gemma Dunlop <1801285@student.uwtsd.ac.uk>
2022-08-10 11:51:25 +0100

    first commit

---
+++ JavaScript/JavaScript.js
@@ -0,0 +1,71 @@
+﻿var navbar = document.getElementById("navbar");
+var sticky = navbar.offsetTop;
+
+function stickyHeader() {
+    if (window.pageYOffset >= sticky) {
+        navbar.classList.add("sticky")
+    } else {
+        navbar.classList.remove("sticky");
+    }
+}
+
+function openFoodOption(foodName) {
+    var i;
+    var x = document.getElementsByClassName("city");
+    for (i = 0; i < x.length; i++) {
+        x[i].style.display = "none";
+    }
+    document.getElementById(foodName).style.display = "block";
+}
+
+function hello() {
+    alert("Thank you. You will receive a text/email to your device shortly.");
+}
+
+function validation() {
+    // First name
+    var thefirstname = document.getElementById('FirstName').value;
+    if (thefirstname == "") {
+        document.getElementById('firstnamemessage').innerHTML = "Please enter your first name.";
+        return false;
+    }
+    else if (thefirstname.length < 2 || thefirstname.length > 30) {
+        document.getElementById('firstnamemessage').innerHTML = "Name too short.";
+        return false;
+    }
+    // Last name
+    var thelastname = document.getElementById('LastName').value;
+    if (thelastname == "") {
+        document.getElementById('lastnamemessage').innerHTML = "Please enter your last name.";
+        return false;
+    }
+    else if (thelastname.length < 2 || thelastname.length > 30) {
+        document.getElementById('lastnamemessage').innerHTML = "Name too short.";
+        return false;
+    }
+    // Phone Number
+    var phoneNumber = document.getElementById('MobileNumber').value;
+    if (phoneNumber == "") {
+        document.getElementById('phonenomessage').innerHTML = "Please enter your mobile number.";
+        return false;
+    }
+    else if (phoneNumber.length != 11) {
+        document.getElementById('phonenomessage').innerHTML = "Mobile number must be 11 numbers.";
+        return false;
+    }
+    if (isNaN(phoneNumber)) {
+        document.getElementById('phonenomessage').innerHTML = "Phone number must not include any letters.";
+        return false;
+    }
+    // Email
+    var theEmail = document.getElementById('email').value;
+    re = new RegExp(/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/);
+    if (re.test(theEmail)) {
+        document.getElementById('emailmessage').innerHTML = "Email OK";
+        return true;
+    }
+    else {
+        document.getElementById('emailmessage').innerHTML = "Incorrect email";
+        return false;
+    }
+}
---
+++ JavaScript/Nutritionix API.js
@@ -0,0 +1,72 @@
+﻿var appID = 'c2aa28e9';
+var appKEY = '624b2d61fe626195c6399369a743bcdb';
+
+function upcGet(upc, callback) {
+    var getURL = "https://api.nutritionix.com/v1_1/item?upc=" + upc + "&appId=" + appID + "&appKey=" + appKEY;
+
+    var xmlHttp = new XMLHttpRequest();
+    xmlHttp.onreadystatechange = function () {
+        if (xmlHttp.readyState == 4 && xmlHttp.status == 200)
+            callback(xmlHttp.responseText);
+    }
+    xmlHttp.open("GET", getURL, true); // true for asynchronous 
+    xmlHttp.send(null);
+}
+
+function postResults(response) {
+    $("[name='results']").html(response);
+}
+
+$(document).on("click", "[name='search']", function () {
+    upcGet($("[name='upc']").val(), postResults);
+})
+
+function postResults(response) {
+    var calories;
+    var fat;
+    var sodium;
+    var sugars;
+
+    var foodItem = JSON.parse(response);
+
+    $("[name='calories']").html(foodItem.nf_calories);
+    $("[name='fat']").html(foodItem.nf_total_fat);
+    $("[name='sodium']").html(foodItem.nf_sodium);
+    $("[name='sugars']").html(foodItem.nf_sugars);
+}
+
+$(document).on("keydown", "[name='upc']", function (e) {
+    console.log(e);
+    if (e.keyCode === 13) {
+        upcGet($("[name='upc']").val(), postResults);
+    }
+});
+
+$(function () {
+    $(".dial").knob();
+});
+
+$('.dial')
+    .val(0)
+    .trigger('change');
+
+function postResults(response) {
+    var calories;
+    var fat;
+    var sodium;
+    var sugars;
+
+    var foodItem = JSON.parse(response);
+
+    $("[name='calories']").val(foodItem.nf_calories);
+    $("[name='calories']").trigger('change');
+    $("[name='fat']").val(foodItem.nf_total_fat);
+    $("[name='fat']").trigger('change');
+    $("[name='sodium']").val(foodItem.nf_sodium);
+    $("[name='sodium']").trigger('change');
+    $("[name='sugars']").val(foodItem.nf_sugars);
+    $("[name='sugars']").trigger('change');
+
+    $("[name='productName']").html(foodItem.item_name);
+
+}
